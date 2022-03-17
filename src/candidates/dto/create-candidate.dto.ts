681cee5886ba3064db95586197f43a127b3850a9
maxi758 <maxi758@gmail.com>
2022-03-17 17:15:59 -0300

    add create-candidate

--- src/candidates/dto/create-candidate.dto.ts
+++ src/candidates/dto/create-candidate.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCandidateDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class CreateCandidateDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+  @IsEmail()
+  email: string;
+}

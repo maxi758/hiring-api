a354c1470bd30b0317da89ea8a4d360db936d58d
maxi758 <maxi758@gmail.com>
2022-03-17 15:02:04 -0300

    create candidate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { AuthModule } from './auth/auth.module';
+import { CandidateModule } from './candidate/candidate.module';
 
 @Module({
-  imports: [CompanyModule, AuthModule],
+  imports: [CompanyModule, AuthModule, CandidateModule],
   controllers: [AppController],
   providers: [AppService],
 })

4cfac4b7dc5fee2ef000dd661a4a6e995ea50e75
maxi758 <maxi758@gmail.com>
2022-03-17 15:12:52 -0300

    correct imported module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompanyModule } from './company/company.module';
 import { AuthModule } from './auth/auth.module';
 import { CandidatesModule } from './candidates/candidates.module';
 import { RecruitersModule } from './recruiters/recruiters.module';
 import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [CompanyModule, AuthModule, CandidatesModule, RecruitersModule, CompaniesModule],
+  imports: [AuthModule, CandidatesModule, RecruitersModule, CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })

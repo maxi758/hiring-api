4c861c47c9ffad8b7d2e1e9ec7b8977a93782709
maxi758 <maxi758@gmail.com>
2022-03-17 15:12:07 -0300

    create resource companies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CompanyModule } from './company/company.module';
 import { AuthModule } from './auth/auth.module';
 import { CandidatesModule } from './candidates/candidates.module';
 import { RecruitersModule } from './recruiters/recruiters.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [CompanyModule, AuthModule, CandidatesModule, RecruitersModule],
+  imports: [CompanyModule, AuthModule, CandidatesModule, RecruitersModule, CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })

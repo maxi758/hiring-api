999a58c1fc768a428d93037bec63f498e39bb5f2
maxi758 <maxi758@gmail.com>
2022-03-17 15:02:48 -0300

    create recruiter resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { AuthModule } from './auth/auth.module';
 import { CandidateModule } from './candidate/candidate.module';
+import { RecruiterModule } from './recruiter/recruiter.module';
 
 @Module({
-  imports: [CompanyModule, AuthModule, CandidateModule],
+  imports: [CompanyModule, AuthModule, CandidateModule, RecruiterModule],
   controllers: [AppController],
   providers: [AppService],
 })

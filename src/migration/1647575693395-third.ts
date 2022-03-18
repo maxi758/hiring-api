9d5a072e7d979fca0dbd907586e0cb3f5834b5db
maxi758 <maxi758@gmail.com>
2022-03-18 01:07:26 -0300

    create migration

---
+++ src/migration/1647575693395-third.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class third1647575693395 implements MigrationInterface {
+    name = 'third1647575693395'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "candidate" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_b0ddec158a9a60fbc785281581b" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "candidate"`);
+    }
+
+}

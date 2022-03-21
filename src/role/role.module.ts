07bbbd7d8740835119c50f284be07fbcd78394f1
maxi758 <maxi758@gmail.com>
2022-03-21 17:45:07 -0300

    correct path to users

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
+import { UsersModule } from '../users/users.module';
 import { Role } from './entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role]), UserModule, AuthModule],
+  imports: [TypeOrmModule.forFeature([Role]), UsersModule, AuthModule],
   exports: [TypeOrmModule],
 })
 export class RoleModule {}

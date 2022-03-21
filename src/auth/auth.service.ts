cb4a162ad69fdc04372dd4aceaf3efe251d75da0
maxi758 <maxi758@gmail.com>
2022-03-21 19:56:53 -0300

    add email resource

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,86 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { UserDto } from '../user/dto/user.dto';
+import { UserPayload } from './dto/user-payload.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  /**
+   *Verifies if user and password are valid
+   *
+   * @param {UserDto} userDto
+   * @return {*}  {Promise<any>}
+   * @memberof AuthService
+   */
+  async validateUser(userDto: UserDto): Promise<UserPayload> {
+    const userInDb = await this.userService.findOneUser(userDto.username);
+    if (userInDb && userDto.password === userInDb.password) {
+      const { password, ...result } = userInDb;
+      return result;
+    }
+    throw new UnauthorizedException('invalid username or password');
   }
-
-  findAll() {
-    return `This action returns all auth`;
+  /**
+   *If user passes validation, signs a token and return it
+   *
+   * @param {UserDto} userDto
+   * @return {*}
+   * @memberof AuthService
+   */
+  async login(userDto: UserDto) {
+    const validUser = await this.validateUser(userDto);
+    const payload = {
+      username: validUser.username,
+      sub: validUser.id,
+      noTimestamp: true,
+    };
+    return {
+      access_token: this.jwtService.sign(payload, {
+        expiresIn: '1h',
+      }),
+    };
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  /**
+   *If a token is valid, returns true. Otherwise throws and exception
+   *
+   * @param {Request} request
+   * @return {*}  {boolean}
+   * @memberof AuthService
+   */
+  permissionControl(request: Request): boolean {
+    try {
+      this.verifyToken(request);
+      return true;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
+  /**
+   *Verifies if token sent in headers is valid
+   *
+   * @param {Request} request
+   * @return {*}
+   * @memberof AuthService
+   */
+  verifyToken(request: Request) {
+    const bearerToken = request.headers['authorization'].split(' ')[1];
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    if (!bearerToken) {
+      throw new UnauthorizedException('No token provided');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return this.jwtService.verify(bearerToken);
   }
 }

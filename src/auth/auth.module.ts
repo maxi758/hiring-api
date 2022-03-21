9b000af1270b12c860b068491cec8c331a82eff1
maxi758 <maxi758@gmail.com>
2022-03-21 20:31:59 -0300

    add auth dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,31 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../users/repositories/user.repository';
 
 @Module({
+  imports: [
+    ConfigModule,
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
+

889ff116e8d1eceed0e0ac1d4bfd2f9cd6ec2585
maxi758 <maxi758@gmail.com>
2022-03-21 20:24:03 -0300

    correct endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,91 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+//import { ValidationGuard } from '../auth/validate.guard';
+//import { ValidateIdDto } from '../common/dto/validate-id';
+//import { ChangeRoleDto } from './dto/change-role.dto';
+//import { Roles } from '../auth/decorators/role.decorator';
+import { AuthGuard } from '@nestjs/passport';
+//import { RoleGuard } from '../auth/guards/role.guard';
+//import { ApiTags, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+//import { ResetPasswordDto } from './dto/reset-password.dto';
+//import { ChangePasswordDto } from './dto/change-password.dto';
 
-@Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+//@ApiTags('user')
+@Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserController {
+  constructor(private usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  //@ApiBearerAuth()
+  @Get('/')
+  //@UseGuards(ValidationGuard)
+  getAll() {
+    return this.usersService.getUsers();
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+  /*@ApiBearerAuth()
+  @Get('/:id')
+  @UseGuards(ValidationGuard)
+  @ApiParam({ name: 'id' })
+  getUserById(@Param() param: ValidateIdDto) {
+    return this.usersService.getUserById(param.id);
+  }*/
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
+  /*@ApiBearerAuth()
+  @Patch('/:id')
+  @UseGuards(ValidationGuard)
+  @ApiParam({ name: 'id' })
+  updateUser(
+    @Body() updateUserDto: UpdateUserDto,
+    @Param() param: ValidateIdDto,
+  ) {
+    return this.usersService.updateUser(param.id, updateUserDto);
+  }*/
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  /*@ApiBearerAuth()
+  @Patch('/:id/role')
+  @Roles('admin')
+  @UseGuards(AuthGuard('jwt'), ValidationGuard, RoleGuard)
+  @ApiParam({ name: 'id' })
+  changeRole(
+    @Body() changeRoleDto: ChangeRoleDto,
+    @Param() param: ValidateIdDto,
+  ) {
+    return this.usersService.changeRole(param.id, changeRoleDto.role);
+  }*/
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  /*@ApiBearerAuth()
+  @Patch('/password/change')
+  @UseGuards(AuthGuard('jwt'), ValidationGuard)
+  changePassword(
+    @Req() request: Request,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    return this.usersService.changePassword(changePasswordDto.password, request);
+  }*/
+  /*@Post('password/reset')
+  resetPassword(@Body() resetPasswordDto: ResetPasswordDto): void {
+    this.usersService.resetPassword(resetPasswordDto.email);
   }
+  @ApiBearerAuth()
+  @Delete('/:id')
+  @UseGuards(ValidationGuard)
+  @ApiParam({ name: 'id' })
+  deleteUser(@Param() param: ValidateIdDto) {
+    this.usersService.removeUser(param.id);
+  }*/
 }

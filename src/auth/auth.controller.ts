14049ef14564368d5888f93c2d412f9de5d5692f
maxi758 <maxi758@gmail.com>
2022-03-22 16:14:36 -0300

    add tags

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,17 +3,17 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { UserDto } from '../users/dto/user.dto';
 import { AuthService } from './auth.service';
-//import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { User } from '../users/entities/user.entity';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 
-//@ApiTags('auth')
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private authService: AuthService,
     private usersService: UsersService,
   ) {}
-  //@ApiBody({ type: UserDto })
+  @ApiBody({ type: UserDto })
   @Post('/login')
   async login(@Body() user: UserDto): Promise<{ access_token: string }> {
     return this.authService.login(user);

349376f223aa6c19715567bf3cb98b262b874be1
maxi758 <maxi758@gmail.com>
2022-03-22 17:42:54 -0300

    fix dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,15 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { UserDto } from './user.dto';
 
-export class CreateUserDto extends UserDto {
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
   @IsEmail()
   email: string;
 }

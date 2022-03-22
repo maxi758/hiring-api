dd27d693ce266512d0ecabde1a3701a9a4a2dbf7
maxi758 <maxi758@gmail.com>
2022-03-22 16:34:44 -0300

    add enum for diferent states of a trainee

---
+++ src/common/dto/enums/stage.enum.ts
@@ -0,0 +1,14 @@
+export enum StagesState {
+  START = 'start',
+  WEEK_1 = 'week-1',
+  WEEK_2 = 'week-2',
+  WEEK_3 = 'week-3',
+  WEEK_4 = 'week-4',
+  WEEK_5 = 'week-5',
+  WEEK_6 = 'week-6',
+  WEEK_7 = 'week-7',
+  WEEK_8 = 'week-8',
+  WEEK_9 = 'week-9',
+  DENIED = 'denied',
+  ACCEPTED = 'accepted',
+}

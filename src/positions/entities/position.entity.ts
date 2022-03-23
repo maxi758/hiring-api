60eab7db9614ffbdaa295ea7a9f2695e7a0eae2f
maxi758 <maxi758@gmail.com>
2022-03-23 19:52:56 -0300

    model position entity

--- src/positions/entities/position.entity.ts
+++ src/positions/entities/position.entity.ts
@@ -1,1 +1,19 @@
-export class Position {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Position {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ name: 'employment_type' })
+  employmentType: string;
+
+  @Column({ name: 'experience_level' })
+  experienceLevel: string;
+
+  @Column()
+  description: string;
+}

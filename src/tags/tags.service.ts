48992a015a5ee2cc7fc1e650a74c8c75e897da57
maxi758 <maxi758@gmail.com>
2022-03-23 15:47:26 -0300

    add methods for crud operations

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class TagsService {
+  constructor(private tagRepository: Repository<Tag>) {}
+
   create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+    return this.tagRepository.save(createTagDto);
   }
 
   findAll() {
-    return `This action returns all tags`;
+    return this.tagRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} tag`;
+    const tag = this.tagRepository.findOne(id);
+    if (!tag) throw new NotFoundException('tag not found');
+    return tag;
   }
 
-  update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
+  async update(id: number, updateTagDto: UpdateTagDto) {
+    const tagToUpdate = await this.findOne(id);
+    if (!tagToUpdate) throw new NotFoundException('tag not found');
+    return this.tagRepository.save({
+      ...tagToUpdate,
+      ...updateTagDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} tag`;
+    return this.tagRepository.delete(id);
   }
 }

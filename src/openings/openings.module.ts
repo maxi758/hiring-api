153130391359a27d4716b0676ffdfaf130d17570
maxi758 <maxi758@gmail.com>
2022-03-23 17:57:38 -0300

    delete resource openings

--- src/openings/openings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OpeningsService } from './openings.service';
-import { OpeningsController } from './openings.controller';
-
-@Module({
-  controllers: [OpeningsController],
-  providers: [OpeningsService]
-})
-export class OpeningsModule {}

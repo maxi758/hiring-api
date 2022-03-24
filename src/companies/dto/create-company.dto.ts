111f80da73b3544fd7c8933d9be77dd6a930f94a
maxi758 <maxi758@gmail.com>
2022-03-24 18:40:34 -0300

    model dto

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCompanyDto {}
+import { Length } from 'class-validator';
+
+export class CreateCompanyDto {
+  @Length(1, 100)
+  name: string;
+  @Length(1, 100)
+  adress: string;
+  @Length(1, 100)
+  city: string;
+  @Length(1, 100)
+  country: string;
+}

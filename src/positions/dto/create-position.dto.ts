9ee6cc40225f1aad9b01b1098edf4efe3fe7e5ba
maxi758 <maxi758@gmail.com>
2022-03-24 19:43:16 -0300

    model dto

--- src/positions/dto/create-position.dto.ts
+++ src/positions/dto/create-position.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePositionDto {}
+import { Length } from 'class-validator';
+
+export class CreatePositionDto {
+  @Length(1, 100)
+  name: string;
+  @Length(1, 50)
+  employmentType: string;
+  @Length(1, 50)
+  experienceLevel: string;
+  @Length(1, 250)
+  description: string;
+}

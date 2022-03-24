9cf30fb57ab749c6d7ce13eb268a046ef4d63779
maxi758 <maxi758@gmail.com>
2022-03-24 18:20:42 -0300

    add imports

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -8,5 +8,6 @@ import { Company } from './entities/company.entity';
   imports: [TypeOrmModule.forFeature([Company])],
   controllers: [CompaniesController],
   providers: [CompaniesService],
+  exports: [CompaniesService],
 })
 export class CompaniesModule {}

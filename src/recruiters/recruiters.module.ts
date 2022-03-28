e67e47f1f47fb2c1a2d6ba55be4f41736d2c5eaf
maxi758 <maxi758@gmail.com>
2022-03-28 16:30:40 -0300

    delete resource recruiter

--- src/recruiters/recruiters.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RecruitersService } from './recruiters.service';
-import { RecruitersController } from './recruiters.controller';
-
-@Module({
-  controllers: [RecruitersController],
-  providers: [RecruitersService]
-})
-export class RecruitersModule {}

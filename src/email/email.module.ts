d6bc248327921e10adb63663b491f12087980bc8
maxi758 <maxi758@gmail.com>
2022-03-29 02:08:15 -0300

    add email service

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email.service';
-import { EmailController } from './email.controller';
 
 @Module({
-  controllers: [EmailController],
-  providers: [EmailService]
+  imports: [ConfigModule],
+  providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as sendgrid from '@sendgrid/mail';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  constructor(private readonly configService: ConfigService) {
+    const key = this.configService.get<string>('SENDGRID_API_KEY');
+    sendgrid.setApiKey(key);
+  }
+  async send(
+    email: string,
+    //transaction: ,
+  ): Promise<void> {
+    try {
+      const msg = {
+        personalizations: [{ to: [{ email }] }],
+        from: 'maxi758@gmail.com',
+        subject: 'transaction details',
+        templateId: this.configService.get<string>('TEMPLATE_ID'),
+        //dynamicTemplateData: { ...transaction },
+      };
+
+      await sendgrid.send(msg);
+    } catch (e) {
+      console.error(e.response.body);
+    }
+  }
+}

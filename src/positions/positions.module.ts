fda42dbe0d04f57565c7111e3308cb7b094fdc20
maxi758 <maxi758@gmail.com>
2022-03-29 19:52:13 -0300

    add missing import

--- src/positions/positions.module.ts
+++ src/positions/positions.module.ts
@@ -4,9 +4,10 @@ import { PositionsController } from './positions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompaniesModule } from '../companies/companies.module';
 import { Position } from './entities/position.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Position]), CompaniesModule],
+  imports: [TypeOrmModule.forFeature([Position]), CompaniesModule, AuthModule],
   controllers: [PositionsController],
   providers: [PositionsService],
   exports: [PositionsService],

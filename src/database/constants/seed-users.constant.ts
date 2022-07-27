aaa7f90d8a32ea73d5447044a54c5a776699a8f5
maxi758 <maxi758@gmail.com>
2022-07-27 12:37:22 -0300

    add users to poblate db

---
+++ src/database/constants/seed-users.constant.ts
@@ -0,0 +1,14 @@
+export const seedUsers = [
+  {
+    username: 'client',
+    email: 'client@client.com',
+    password: '1234',
+    role: 'client',
+  },
+  {
+    username: 'admin',
+    email: 'admin@admin.com',
+    password: '1234',
+    role: 'admin',
+  },
+];

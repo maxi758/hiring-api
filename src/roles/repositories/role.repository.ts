bd352dddf0ca6c981701d413c93f603b144e67ec
maxi758 <maxi758@gmail.com>
2022-07-27 12:40:31 -0300

    fix custum repository to replace old repository

--- src/roles/repositories/role.repository.ts
+++ src/roles/repositories/role.repository.ts
@@ -1,19 +1,28 @@
-import { EntityRepository, Repository, In } from 'typeorm';
+import { Repository, In } from 'typeorm';
+import { AppDataSource } from '../../data-source';
 import { Role } from '../entities/role.entity';
 
-@EntityRepository(Role)
-export class RoleRepository extends Repository<Role> {
+export interface RoleRepository extends Repository<Role> {
+  this: Repository<Role>;
+  seedRoles(roles: string[]): Promise<Role[]>;
+  findOneRole(criteria: any): Promise<Role>;
+  findRolebyName(roleName: string): Promise<Role>;
+}
+
+export const customRoleRepository = AppDataSource.getRepository(Role).extend({
   async seedRoles(roles: string[]): Promise<Role[]> {
     const savedRoles = (await this.find({ name: In(roles) })).map(
-      role => role.name,
+      (role) => role.name,
     );
 
-    if (!roles.every(role => savedRoles.includes(role))) {
-      return this.save(roles.map(role => ({ name: role })));
+    if (!roles.every((role) => savedRoles.includes(role))) {
+      return this.save(roles.map((role) => ({ name: role })));
     }
-  }
-
+  },
+  async findOneRole(criteria: any): Promise<Role> {
+    return this.findOne(criteria);
+  },
   findRolebyName(roleName: string): Promise<Role> {
     return this.findOne({ name: roleName });
-  }
-}
+  },
+});
